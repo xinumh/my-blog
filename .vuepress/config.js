b859d583813ef500099b0f724810ea9a59204db0
hx <huangxin@wondertek.com.cn>
2021-03-04 17:11:32 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -52,7 +52,7 @@ module.exports = {
         "items": [
           {
             "text": "GitHub",
-            "link": "https://github.com/recoluan",
+            "link": "https://github.com/xinumh",
             "icon": "reco-github"
           }
         ]
