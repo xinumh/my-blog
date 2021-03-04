286e7d9bd03ab7a87f2b625311d1a61be52041f2
hx <huangxin@wondertek.com.cn>
2021-03-04 14:22:34 +0800

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "title": "xxqw",
   "description": "",
-  "dest": "public",
+  "dest": "dist",
   "head": [
     [
       "link",
--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,17 @@ npm run build
 # git init
 git add .
 git commit -m 'deploy'
-
-git push -f origin  master:master
+git push
 
 # 进入生成的文件夹
-cd public
+cd dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:xinumh/xinumh.github.io.git master
