86e127036c86555428e44faddb698dc6f2ba985d
hx <huangxin@wondertek.com.cn>
2021-03-04 14:15:30 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,17 @@ set -e
 # 生成静态文件
 npm run build
 
-# 进入生成的文件夹
-# cd public
-
 # git init
 git add .
 git commit -m 'deploy'
 
+git push -f origin  master:master
+
+# 进入生成的文件夹
+cd public
+
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:xinumh/xinumh.github.io.git master
-
-git push -f origin  master:master
 
 cd ..
 
